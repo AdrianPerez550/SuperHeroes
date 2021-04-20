163c2551c4befeec2043fe03184367dc443d8f36
AdrianPerez550 <201822143@tese.edu.mx>
2021-04-20 15:09:54 -0500

    Cambio sobre las carpetas  contador, heroe y listado

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,27 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css'],
+})
+export class HeroeComponent {
+
+  nombre: string = 'Saitama';
+  edad: number = 30;
+
+
+  obtenerNombre(): string{
+    return this.nombre + ' - '+ this.edad
+  }
+
+  cambiarNombre(): void{
+    this.nombre = 'Superman';
+  }
+
+  cambiarEdad(): void {
+    this.edad = 38;
+  }
+
+}
